7afc5a0d09731ef4d649fa5411dc6e39d4de856b
András Veres-Szentkirályi <vsza@vsza.hu>
2020-01-26 20:04:16 +0100

    added CSV

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,13 @@ use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
 use dotenv::dotenv;
 use std::env;
+use std::fmt::Write;
 use std::collections::HashMap;
 
 pub mod schema;
 pub mod models;
+
+const CSV_EVENT_LENGTH: usize = 59;
 
 use models::*;
 
@@ -68,6 +71,10 @@ pub fn get_history() -> Vec<Event> {
     events.order(when).load::<Event>(&connection).unwrap()
 }
 
+pub fn event_to_csv(dst: & mut String, src: &Event) -> Result<(), std::fmt::Error> {
+    write!(dst, "{};{};{}\n", src.id, src.when, if src.what { '1' } else { '0' })
+}
+
 async fn home(_query: web::Query<HashMap<String, String>>) -> Result<HttpResponse> {
     Ok(HttpResponse::Ok().content_type("text/html").body(Home.render().unwrap()))
 }
@@ -80,6 +87,12 @@ async fn status_json(_query: web::Query<HashMap<String, String>>) -> Result<Http
 async fn status_xml(_query: web::Query<HashMap<String, String>>) -> Result<HttpResponse> {
     let last = get_last_event();
     Ok(HttpResponse::Ok().content_type("text/xml").body(last.render().unwrap()))
+}
+
+async fn status_csv(_query: web::Query<HashMap<String, String>>) -> Result<HttpResponse> {
+    let mut csv = String::with_capacity(CSV_EVENT_LENGTH);
+    event_to_csv(&mut csv, &get_last_event());
+    Ok(HttpResponse::Ok().content_type("text/csv").body(csv))
 }
 
 async fn status_txt(_query: web::Query<HashMap<String, String>>) -> Result<HttpResponse> {
@@ -102,6 +115,15 @@ async fn history_xml(_query: web::Query<HashMap<String, String>>) -> Result<Http
     Ok(HttpResponse::Ok().content_type("text/xml").body(tpl.render().unwrap()))
 }
 
+async fn history_csv(_query: web::Query<HashMap<String, String>>) -> Result<HttpResponse> {
+    let history = get_history();
+    let mut csv = String::with_capacity(history.len() * CSV_EVENT_LENGTH);
+    for event in history {
+        event_to_csv(&mut csv, &event);
+    }
+    Ok(HttpResponse::Ok().content_type("text/csv").body(csv))
+}
+
 async fn history_html(_query: web::Query<HashMap<String, String>>) -> Result<HttpResponse> {
     let tpl = HistoryHTML { history: get_history() };
     Ok(HttpResponse::Ok().content_type("text/html").body(tpl.render().unwrap()))
@@ -115,9 +137,11 @@ async fn main() -> std::io::Result<()> {
 			.service(web::resource("/").route(web::get().to(home)))
 			.service(web::resource("/status.json").route(web::get().to(status_json)))
 			.service(web::resource("/status.txt").route(web::get().to(status_txt)))
+			.service(web::resource("/status.csv").route(web::get().to(status_csv)))
 			.service(web::resource("/status.xml").route(web::get().to(status_xml)))
 			.service(web::resource("/status").route(web::get().to(status_html)))
 			.service(web::resource("/history.json").route(web::get().to(history_json)))
+			.service(web::resource("/history.csv").route(web::get().to(history_csv)))
 			.service(web::resource("/history.xml").route(web::get().to(history_xml)))
 			.service(web::resource("/history").route(web::get().to(history_html)))
     })
