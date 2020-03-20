1360379c15e765f16c711bbba338419275150d1c
András Veres-Szentkirályi <vsza@vsza.hu>
2020-03-20 22:15:44 +0100

    changed listen port to 8000

--- src/main.rs
+++ src/main.rs
@@ -295,7 +295,7 @@ async fn main() -> std::io::Result<()> {
 			.service(web::resource("/history.xml").route(web::get().to(history_xml)))
 			.service(web::resource("/history").route(web::get().to(history_html)))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("127.0.0.1:8000")?
     .run()
     .await
 }
